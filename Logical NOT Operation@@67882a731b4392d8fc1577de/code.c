70142f979c635fe6d9c9e66f21063336e992dfef
CLASHERFTW <103025942+CLASHERFTW@users.noreply.github.com>
2025-02-12 10:33:09 +0530

    Solution to Logical NOT Operation

---
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void compareNumbers(int a) {
+    if (a!>0) {
+        printf("True");
+    } 
+    
+}
+
+int main() {
+    int num1, num2;
+    
+    
+    scanf("%d", &num1);
+    
+   
+   
+    
+    compareNumbers(num1, num2);
+
+    return 0;
+}
