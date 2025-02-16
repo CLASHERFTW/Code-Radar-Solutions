f1d7b351ceaa032273070b4db9c0a0126c9aa779
CLASHERFTW <103025942+CLASHERFTW@users.noreply.github.com>
2025-02-16 17:39:37 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -1,24 +1,12 @@
-#include <stdio.h>
-
-void compareNumbers(int a, int b) {
-    if (a||b>0) {
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>0 || b>0){
         printf("True");
-    } else   {
+    }
+    else{
         printf("False");
-    } 
-    
-}
-
-int main() {
-    int num1, num2;
-    
-    
-    scanf("%d", &num1);
-    
-   
-    scanf("%d", &num2);
-    
-    compareNumbers(num1, num2);
-
+    }
     return 0;
 }
