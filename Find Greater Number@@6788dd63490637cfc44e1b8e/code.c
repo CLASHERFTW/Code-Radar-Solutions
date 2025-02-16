d35173215163ac38ac0002424ab2c6769f5818b1
CLASHERFTW <103025942+CLASHERFTW@users.noreply.github.com>
2025-02-16 11:00:14 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -5,10 +5,10 @@ int main(){
     scanf("%d",&a);
     scanf("%d",&b);
     if(a>b){
-        printf("%d",a);
+        printf("%d",a);}
     else{
         printf("%d",b);
         }
-    }
+    
     return 0;
 }
