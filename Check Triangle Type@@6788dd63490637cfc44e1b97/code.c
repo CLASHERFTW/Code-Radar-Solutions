e5bd4270e8a9ec96e07c01b0296cd953ff7a6c71
CLASHERFTW <103025942+CLASHERFTW@users.noreply.github.com>
2025-02-16 11:47:31 +0530

    Solution to Check Triangle Type

--- Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -1,16 +1,22 @@
-#include<stdio.h>
-int main(){
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    if(a==b&&b==c&&c==a)
-    {
-        printf("equilateral");
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    // Taking input
+    printf("Enter three sides of the triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Checking the type of triangle
+    if (a == b && b == c) {  // All three sides are equal
+        printf("Equilateral\n");
     }
-    else if(a==b && b==c||c==a && c==b ){
-        printf("Isoceles");
+    else if (a == b || b == c || c == a) {  // At least two sides are equal
+        printf("Isosceles\n");
     }
-    else{
-        printf("scalene");
+    else {  // No sides are equal
+        printf("Scalene\n");
     }
+
     return 0;
 }
